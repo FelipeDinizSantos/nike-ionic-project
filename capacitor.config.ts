84c7f05fa546dc9dab8586d3ae86ff17625f2471
Felipe Diniz <felipedinizdossantos@gmail.com>
2024-05-04 17:10:02 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Nike-Application-Design',
+  webDir: 'dist'
+};
+
+export default config;
