ae474c53a1f4e1674a5d22176cf66ffa281f0744
Felipe Diniz <felipedinizdossantos0@gmail.com>
2024-05-06 19:48:40 -0300

    fix styles

--- src/components/home/Content.tsx
+++ src/components/home/Content.tsx
@@ -6,8 +6,8 @@ const Content: React.FC<ContainerProps> = () => {
   return (
     <div className='content-container'>
         <h1 className='content-title'>SUMMER<br/>COLECTIONS</h1>
-        <h2 className='content-sub-title'>2022</h2>
-        <p className='content-paragraph'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa quod ullam nihil facilis sint laborum illo qui sed perspiciatis voluptas magnam commodi.</p>
+        <h2 className='content-sub-title'>2024</h2>
+        <p className='content-paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem natus quia asperiores similique voluptas veritatis sint dicta quam. Iure quis blanditiis odio incidunt non veniam corporis delectus, culpa asperiores quas.</p>
     </div>
   );
 };
